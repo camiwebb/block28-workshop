6e8a074dc46bbec952ed85ae78b15227450dc439
Camille Webb <camillewebb35@gmail.com>
2024-10-17 16:58:46 -0600

    created the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import { useState } from 'react'
-import './App.css'
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import Blue from './components/Blue';
+import Red from './components/Red';
+import Home from './components/Home';
+import "./index.css";
+
 
 function App() {
 
   return (
     <>
       <div id="container">
-        <div id="navbar">{/* navigation here */}</div>
+        <div id="navbar">
+          <Link to="/">Home</Link>
+          <Link to="/blue">Blue</Link>
+          <Link to="/red">Red</Link>
+        </div>
         <div id="main-section">
           <Routes>
-            <Route path="/blue" element={<h1>Blue</h1>}/>
-            <Route path="/red" element={<h1>Red</h1>}/>
+            <Route path="/" element={<Home />}/>
+            <Route path="/blue" element={<Blue />}/>
+            <Route path="/red" element={<Red />}/>
           </Routes>
         </div>
       </div>
